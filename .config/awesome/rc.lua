24efaa9ac26cf6caea67174480bd606ec4cf6163
xceejay <joelkofiamoako@gmail.com>
2019-10-26 15:36:34 +0000

    added some things to bashrc and added a wifi widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -87,9 +87,10 @@ local scrlocker    = "slock"
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5","6" }
 awful.layout.layouts = {
-    awful.layout.suit.floating,
+   
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
+    awful.layout.suit.floating,
   --  awful.layout.suit.tile.bottom,
    -- awful.layout.suit.tile.top,
    -- awful.layout.suit.fair,
