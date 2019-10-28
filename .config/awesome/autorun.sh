414a139999772e6421fdd5cf23c713449def080d
xceejay <joelkofiamoako@gmail.com>
2019-10-28 17:59:10 +0000

    better pulse audio changes

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -6,10 +6,11 @@ function run {
     $@&
   fi
 }
-killall pulseaudio
+
 run compton
 run sxhkd
 run xfce4-clipman
 run xfce4-terminal --drop-down
 run pasystray -a
-run pulseaudio --start
+run pulseaudio
+
