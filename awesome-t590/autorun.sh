f2e1429cb5f8174d0c36e9ce76e581d0f1642d54
xceejay <joelkofiamoako@gmail.com>
2022-06-01 12:17:39 +0000

    add

---
+++ awesome-t590/autorun.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+xset  r rate 300 50
+#autocutsel &
+#autocutsel -s PRIMARY &
+
+function run {
+  if ! pgrep -f "$1" ;
+  then
+    $@&
+  fi
+}
+
+run picom
+run sxhkd
+run clipmenud
+#run xfce4-clipman
+#run xfce4-terminal --drop-down
+#run pnmixer
+#run volumeicon
+# run volti ##run pasystray -a
+run pasystray -a
+run nm-applet
+run unclutter
+#run pulseaudio --start
+#run pulseaudio
+#run devilspie
+#run transmission-gtk
+#run kdeconnect-indicator
+run blueman-applet
+#run clementine
+#run flashfocus
+#run rhythmbox
+#run xdman
+#tgceejay="/home/joel/Telegram/Telegram -many -workdir ~/ceejay" && run "$tgceejay"
+
